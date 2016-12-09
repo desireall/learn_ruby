671773fc4f98e11d1583c6ad5f32b9d79aeae0f4
desireall <yu_huo_y@163.com>
2016-12-09 18:02:10 +0800

    0~4

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -3,13 +3,17 @@
 $vowelSound = Array.[]("a","e","i","o","u");
 def translateWord(str)  
     index = 0
-     i = 0;
-    while i < str.size do
+    i = 0;
+    if str.include?("qu")
+      index = str.index("qu") + 2
+    else
+     while i < str.size do
        if $vowelSound.include?(str[i])
          index = i
          break
        end
        i += 1
+     end
     end
     # 正则
     if index == 0
@@ -34,3 +38,7 @@ puts translate("the quick brown fox")
 puts translate("quiet")
 
 puts "asdasd"[0]
+
+#puts "quiet".include?("qu");
+
+puts "quiet"["quiet".index("qu") + 2 , 4]
